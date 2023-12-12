430ce36602755c46278730ab9d263c4e3ba7a8cc
Luisa Ramirez <luisa.ramirez3@utp.edu.co>
2023-12-11 23:36:24 -0500

    Add database initialization code

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from config.database import engine, Base
 
 app = FastAPI()
 
 app.title = "Recomendador de Contraseñas"
 app.version = "1.0.0"
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/",tags=['home'])
 def message():
---
+++ Backend/routers/recommender.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter, Depends, Path, Query
+from fastapi import JSONResponse
+from typing import List
+from config.database import sesion 
